1e763e3664c6cb60b24556863d8b03b62006ba61
KelsCree <kelseycreehan@gmail.com>
2020-10-08 15:10:40 -0600

    -mbuild alive button functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import Characters from './components/Characters'
 
 class App extends React.Component {
   
@@ -9,6 +10,20 @@ class App extends React.Component {
     deceased: []
   }
 
+  addToAlive = (character) => {
+    if (!this.state.alive.find(aliveCharacter => aliveCharacter === character)) {
+    this.setState({ alive: [...this.state.alive, character] })
+    const updatedCharacters = this.state.characters.filter(charac => charac !== character)
+    this.setState({ characters: updatedCharacters})
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://breakingbadapi.com/api/characters')
+      .then(response => response.json())
+      .then(characters => this.setState({ characters }))
+  }
+
   render(){
     return (
       <div className="App">
@@ -16,10 +31,16 @@ class App extends React.Component {
         <main>
           <section className='all-characters'>
             <h2>All Characters</h2>
+            <Characters 
+              characters={this.state.characters} 
+              addtoAlive={this.addToAlive}/>
           </section>
           <div className='sorted-characters'>
             <section className='alive'>
               <h2>Alive</h2>
+              <Characters 
+                characters={this.state.alive}
+              />
             </section>
             <section className='deceased'>
               <h2>Deceased</h2>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function Card(){
+export default function Card({ character, addToAlive }){
+
+  const { name } = character
+
+  const handleAliveClick = () => {
+    addToAlive(character)
+  }
 
   return (
     <div className='card'>
-      <h3>Character's Name</h3>
-      <button>Alive</button>
+      <h3>{name}</h3>
+      <button onClick={handleAliveClick}>Alive</button>
       <button>Deceased</button>
     </div>
   )
